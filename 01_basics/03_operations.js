eb99093831e7ee825a448df085f1c4dd5e6bf1b1
Dipak Digambar Lande <128961865+DipakLande@users.noreply.github.com>
2024-08-03 07:45:22 +0000

    01_basics

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,16 @@
+// let a = 3
+// let b = 5
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(a ** b);
+
+
+let str1 = "Hello"
+let str2 = "Dipak"
+
+let str3 = (str1 + str2)
+console.log(str3);
---
+++ 01_basics/Data Type Conversion.js
@@ -0,0 +1,19 @@
+ let score = 32
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = boolean(isLoggedIn)
+console.log();
+
+
