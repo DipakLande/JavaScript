3262da542a70531f83d900b4ac8bc1291fc1f93f
Dipak Digambar Lande <128961865+DipakLande@users.noreply.github.com>
2024-08-14 15:11:45 +0000

    iteration

---
+++ 05_Iterations/one.js
@@ -0,0 +1,18 @@
+// for
+
+// for (let i = 0; i < 10; index++) {
+//     const element = i;
+//     // console.log(element);
+    
+// }
+
+for (let i = 0; i <= 10; i++) {
+        console.log(`outer Loop value: ${i}`);
+    
+    for (let j = 0; j < 10; j++) {
+        // console.log(`inner Loop value: ${j} and inner loop ${i}`);
+        
+        
+    }
+    
+}
